ad01d42506de1e7e9461763dcc01734f5e73948f
tartarJR <mustafaogun.ozturk@gmail.com>
2019-06-07 02:01:23 +0300

    Add missing methods of brief presenter

--- app/src/main/java/com/tatar/weatherify/ui/brief/BriefWeatherMvpPresenter.kt
+++ app/src/main/java/com/tatar/weatherify/ui/brief/BriefWeatherMvpPresenter.kt
@@ -4,4 +4,6 @@ import com.tatar.weatherify.ui.base.BaseMvpPresenter
 
 interface BriefWeatherMvpPresenter : BaseMvpPresenter<BriefWeatherMvpView?> {
     fun retrieveWeatherForecastInformation()
+    fun clearDisposable()
+    fun disposeDisposable()
 }
