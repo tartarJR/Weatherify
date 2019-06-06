a573cc2a0fbb5bd9985d5a3f7f01bc0b7fd50451
tartarJR <mustafaogun.ozturk@gmail.com>
2019-06-07 02:19:50 +0300

    Add WeatherActivity implementation

--- app/src/main/java/com/tatar/weatherify/ui/brief/BriefWeatherActivity.kt
+++ app/src/main/java/com/tatar/weatherify/ui/brief/BriefWeatherActivity.kt
@@ -1,31 +1,75 @@
 package com.tatar.weatherify.ui.brief
 
+import android.view.View
+import com.tatar.weatherify.App
+import com.tatar.weatherify.R
 import com.tatar.weatherify.data.network.model.WeatherForecastResponse
+import com.tatar.weatherify.di.brief.component.DaggerBriefComponent
 import com.tatar.weatherify.ui.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_brief_weather.*
+import javax.inject.Inject
 
 class BriefWeatherActivity : BaseActivity(), BriefWeatherMvpView {
 
-    override fun displayWeatherForecastInformation(weatherForecastResponse: WeatherForecastResponse) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    @Inject
+    lateinit var briefWeatherMvpPresenter: BriefWeatherMvpPresenter
 
     override fun getLayoutId(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return R.layout.activity_brief_weather
     }
 
     override fun provideDependencies() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val searchComponent = DaggerBriefComponent.builder()
+            .briefWeatherActivity(this)
+            .appComponent(App.appInstance.appComponent()).build()
+
+        searchComponent.injectSearchActivity(this)
     }
 
     override fun initViews() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun init() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        briefWeatherMvpPresenter.attachView(this)
+        briefWeatherMvpPresenter.retrieveWeatherForecastInformation()
     }
 
     override fun releasePresenterResources() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        briefWeatherMvpPresenter.detachView()
+        briefWeatherMvpPresenter.disposeDisposable()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        briefWeatherMvpPresenter.clearDisposable()
+    }
+
+    override fun showTitle() {
+        four_days_weather_title_tv.visibility = View.VISIBLE
+    }
+
+    override fun hideTitle() {
+        four_days_weather_title_tv.visibility = View.GONE
+    }
+
+    override fun showBriefWeatherCompoundViews() {
+        first_daily_weather_brief_view.visibility = View.VISIBLE
+        second_daily_weather_brief_view.visibility = View.VISIBLE
+        third_daily_weather_brief_view.visibility = View.VISIBLE
+        fourth_daily_weather_brief_view.visibility = View.VISIBLE
+    }
+
+    override fun hideBriefWeatherCompoundViews() {
+        first_daily_weather_brief_view.visibility = View.GONE
+        second_daily_weather_brief_view.visibility = View.GONE
+        third_daily_weather_brief_view.visibility = View.GONE
+        fourth_daily_weather_brief_view.visibility = View.GONE
+    }
+
+    override fun displayWeatherForecastInformation(weatherForecastResponse: WeatherForecastResponse) {
+        first_daily_weather_brief_view.setDailyWeather(weatherForecastResponse.forecasts[0])
+        second_daily_weather_brief_view.setDailyWeather(weatherForecastResponse.forecasts[1])
+        third_daily_weather_brief_view.setDailyWeather(weatherForecastResponse.forecasts[2])
+        fourth_daily_weather_brief_view.setDailyWeather(weatherForecastResponse.forecasts[3])
     }
 }
