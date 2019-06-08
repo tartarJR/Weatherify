0857dc56f430e4efd0bb8850f0b1f893c6c1981e
tartarJR <mustafaogun.ozturk@gmail.com>
2019-06-08 21:20:47 +0300

    Remove unused methods, properties from DateUtil

--- app/src/main/java/com/tatar/weatherify/util/DateUtil.kt
+++ app/src/main/java/com/tatar/weatherify/util/DateUtil.kt
@@ -5,13 +5,7 @@ import java.util.*
 
 object DateUtil {
 
-    private val dayFormat = SimpleDateFormat("EEEE", Locale.ENGLISH)
-    private val dateFormat = SimpleDateFormat("dd MMM yyyy", Locale.ENGLISH)
-
-    @JvmStatic
-    fun getDay(date: Date): String {
-        return dayFormat.format(date)
-    }
+    private val dateFormat = SimpleDateFormat("EE, dd MMM yyyy", Locale.ENGLISH)
 
     @JvmStatic
     fun getFormattedDate(date: Date): String {
