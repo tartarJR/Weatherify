c4f6c37835b25313b47b3768ec2096b2f4d93897
Mustafa Ogün Öztürk <mustafaogun.ozturk@gmail.com>
2019-06-13 01:19:35 +0300

    Add temperature view

---
+++ app/src/main/java/com/tatar/weatherify/ui/base/TemperatureCompoundView.kt
@@ -0,0 +1,30 @@
+package com.tatar.weatherify.ui.base
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.tatar.weatherify.R
+import kotlinx.android.synthetic.main.view_temperature.view.*
+
+class TemperatureCompoundView : ConstraintLayout {
+
+    private var temperature: Double = 0.0
+
+    constructor(context: Context) : this(context, null)
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    init {
+        LayoutInflater.from(context).inflate(R.layout.view_temperature, this, true)
+    }
+
+    fun setTemperature(temperature: Double) {
+        this.temperature = temperature
+        setupView()
+    }
+
+    private fun setupView() {
+        temperature_tv.text = temperature.toString()
+    }
+}
