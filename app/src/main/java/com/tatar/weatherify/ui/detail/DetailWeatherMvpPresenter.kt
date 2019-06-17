72365a3b5dd5799d1023a39896dd708f78679e00
Mustafa Ogün Öztürk <mustafaogun.ozturk@gmail.com>
2019-06-17 20:11:07 +0300

    Add DetailWeatherMvpPresenter and its implementation

--- app/src/main/java/com/tatar/weatherify/ui/detail/DetailWeatherMvpPresenter.kt
+++ app/src/main/java/com/tatar/weatherify/ui/detail/DetailWeatherMvpPresenter.kt
@@ -1,6 +1,8 @@
 package com.tatar.weatherify.ui.detail
 
+import com.tatar.weatherify.data.network.model.DailyWeather
 import com.tatar.weatherify.ui.base.BaseMvpPresenter
 
 interface DetailWeatherMvpPresenter : BaseMvpPresenter<DetailWeatherMvpView?> {
+    fun displayDetailWeatherInformation(dailyWeather: DailyWeather, isDay: Boolean)
 }
--- app/src/main/java/com/tatar/weatherify/ui/detail/DetailWeatherPresenter.kt
+++ app/src/main/java/com/tatar/weatherify/ui/detail/DetailWeatherPresenter.kt
@@ -1,12 +1,61 @@
 package com.tatar.weatherify.ui.detail
 
+import com.tatar.weatherify.data.network.model.DailyWeather
+import com.tatar.weatherify.data.network.model.WeatherInfo
+import java.util.*
+
 class DetailWeatherPresenter : DetailWeatherMvpPresenter {
 
+    private var detailWeatherMvpView: DetailWeatherMvpView? = null
+
+    override fun displayDetailWeatherInformation(dailyWeather: DailyWeather, isDay: Boolean) {
+        if (isDay) {
+            detailWeatherMvpView?.setBgColorToDay()
+            detailWeatherMvpView?.setSwitchTextToDay()
+            displayDayOrNightWeatherInfo(dailyWeather.date, dailyWeather.day)
+        } else {
+            detailWeatherMvpView?.setBgColorToNight()
+            detailWeatherMvpView?.setSwitchTextToNight()
+            displayDayOrNightWeatherInfo(dailyWeather.date, dailyWeather.night)
+        }
+    }
+
     override fun attachView(view: DetailWeatherMvpView?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        this.detailWeatherMvpView = view
     }
 
     override fun detachView() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        this.detailWeatherMvpView = null
+    }
+
+    private fun displayDayOrNightWeatherInfo(date: Date, dayOrNightInfo: WeatherInfo) {
+
+        detailWeatherMvpView?.displayWeatherInfoContainer(
+            date,
+            dayOrNightInfo.tempmax,
+            dayOrNightInfo.tempmin,
+            dayOrNightInfo.phenomenon,
+            dayOrNightInfo.text
+        )
+
+        dayOrNightInfo.places?.let {
+            if (it.isNotEmpty()) detailWeatherMvpView?.displayPlacesContainer(it)
+            else detailWeatherMvpView?.hidePlacesContainer()
+        }
+
+        dayOrNightInfo.winds?.let {
+            if (it.isNotEmpty()) detailWeatherMvpView?.displayWindsContainer(it)
+            else detailWeatherMvpView?.hideWindsContainer()
+        }
+
+        dayOrNightInfo.sea?.let {
+            if (it.isNotBlank()) detailWeatherMvpView?.displaySeaContainer(it)
+            else detailWeatherMvpView?.hideSeaContainer()
+        }
+
+        dayOrNightInfo.peipsi?.let {
+            if (it.isNotBlank()) detailWeatherMvpView?.displayPeipsiContainer(it)
+            else detailWeatherMvpView?.hidePeipsiContainer()
+        }
     }
 }
