a88b943973cf2cd752d38a936d2ed6cf9c2a1968
Mustafa Ogün Öztürk <mustafaogun.ozturk@gmail.com>
2019-06-17 20:11:42 +0300

    Add provides method for DetailWeatherMvpPresenter

--- app/src/main/java/com/tatar/weatherify/di/detail/DetailModule.kt
+++ app/src/main/java/com/tatar/weatherify/di/detail/DetailModule.kt
@@ -1,5 +1,7 @@
 package com.tatar.weatherify.di.detail
 
+import com.tatar.weatherify.ui.detail.DetailWeatherMvpPresenter
+import com.tatar.weatherify.ui.detail.DetailWeatherPresenter
 import com.tatar.weatherify.ui.detail.PlaceAdapter
 import com.tatar.weatherify.ui.detail.WindAdapter
 import dagger.Module
@@ -20,5 +22,12 @@ object DetailModule {
     @Provides
     fun windAdapter(): WindAdapter {
         return WindAdapter()
+    }
+
+    @JvmStatic
+    @PerDetail
+    @Provides
+    fun detailWeatherPresenter(): DetailWeatherMvpPresenter {
+        return DetailWeatherPresenter()
     }
 }
