c38773382f64a6c489a63e5b74037932764cc2fb
tartarJR <mustafaogun.ozturk@gmail.com>
2019-06-21 22:13:45 +0300

    Refactor place weather item based on requirements

--- app/src/main/java/com/tatar/weatherify/ui/detail/PlaceAdapter.kt
+++ app/src/main/java/com/tatar/weatherify/ui/detail/PlaceAdapter.kt
@@ -6,10 +6,8 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.tatar.weatherify.R
 import com.tatar.weatherify.data.network.model.Place
-import com.tatar.weatherify.util.ViewUtil
 import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.place_weather_item.*
-import kotlinx.android.synthetic.main.view_brief_weather.view.*
 import javax.inject.Inject
 
 class PlaceAdapter @Inject constructor(private val itemClickListener: ItemClickListener) :
@@ -35,14 +33,23 @@ class PlaceAdapter @Inject constructor(private val itemClickListener: ItemClickL
         notifyDataSetChanged()
     }
 
-    inner class PlaceViewHolder(override val containerView: View) :
-        RecyclerView.ViewHolder(containerView),
+    inner class PlaceViewHolder(override val containerView: View) : RecyclerView.ViewHolder(containerView),
         LayoutContainer {
 
         fun bind(place: Place, itemClickListener: ItemClickListener) = with(itemView) {
-            place_name_tv.text = place.name
-            phenomenon_tv.text = place.phenomenon
-            phenomenon_iv.setImageResource(ViewUtil.getWeatherIconByPhenomenon(place.phenomenon))
+            val context = containerView.context
+
+            name_tv.text = place.name
+
+            max_temp_tv.text = if (place.tempmax != null) context.getString(
+                R.string.max_temp_tv_txt,
+                place.tempmax
+            ) else context.getString(R.string.max_temp_tv_na_txt)
+
+            min_temp_tv.text = if (place.tempmin != null) context.getString(
+                R.string.min_temp_tv_txt,
+                place.tempmin
+            ) else context.getString(R.string.min_temp_tv_na_txt)
 
             setOnClickListener { itemClickListener.onItemClick(place) }
         }
